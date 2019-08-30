d2ef5721ca5d6d8f7d325826a8716bb336568b8e
Safoora Yousefi <safoora.yousefi1192@gmail.com>
2019-08-30 03:17:49 +0000

    saves scaler

--- gru.py
+++ gru.py
@@ -1,17 +1,17 @@
 from augmentation import resample_augment
 from data_utils import label_shift, prepare_data
+import numpy as np
+import pandas as pd
+import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-import pandas as pd
-import numpy as np
-from lstm_utils import temporalize, flatten, scale
+from time_series_utils import temporalize, flatten, scale
 from tensorflow.keras import optimizers, Sequential
 from tensorflow.keras.models import Model
-from tensorflow.keras.metrics import FalseNegatives, FalsePositives, TruePositives, TrueNegatives, Recall 
+from tensorflow.keras.metrics import FalseNegatives, FalsePositives, TruePositives, TrueNegatives, Recall, Precision 
 from tensorflow.keras.utils import plot_model
-from tensorflow.keras.layers import Dense, GRU, LSTM, RepeatVector, TimeDistributed
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.layers import Dense, GRU, LSTM, Reshape
 
 SEED = 0
 DATA_SPLIT_PCT = 0.20
@@ -19,7 +19,7 @@ LOOKBACK=20
 
 input_X, input_y = prepare_data()
 
-n_features = input_X.shape[1]  # number of features
+n_features = input_X.shape[1]
 
 X, y = temporalize(input_X, input_y, LOOKBACK)
 
@@ -33,8 +33,14 @@ X_train = X_train.reshape(X_train.shape[0], LOOKBACK, n_features)
 X_test = X_test.reshape(X_test.shape[0], LOOKBACK, n_features)
 
 
-#X_train, y_train = resample_augment(X_train, y_train)
+X_train, y_train = resample_augment(X_train, y_train)
 X_train, y_train = shuffle(X_train, y_train, random_state=SEED)
+
+print(X_train.shape, X_test.shape)
+x_pos = X_train[y_train == 1]
+frac = X_train.shape[0] // x_pos.shape[0]
+print(frac)
+
 
 scaler = StandardScaler().fit(flatten(X_train))
 X_train_scaled = scale(X_train, scaler)
@@ -42,23 +48,34 @@ X_train_scaled = scale(X_train, scaler)
 X_test_scaled = scale(X_test, scaler)
 
 
-epochs = 200
+epochs = 1
 batch = 128
 lr = 0.0001
+hidden_size = 128
 
-lstm_model = Sequential()
-lstm_model.add(GRU(units=128, activation='relu', return_sequences=False,
+gru_model = Sequential()
+gru_model.add(GRU(units=hidden_size, activation='relu', return_sequences=True,
                dropout = 0.5, recurrent_dropout=0.5, input_shape=(LOOKBACK, n_features)))
-#lstm_model.add(LSTM(units=256, activation='relu', return_sequences=False))
-lstm_model.add(Dense(1, activation='sigmoid'))
-lstm_model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=lr),
-                   metrics=['accuracy', FalsePositives(), FalseNegatives(), TruePositives(), TrueNegatives(), Recall()])
-print(lstm_model.summary())
+gru_model.add(Reshape((LOOKBACK*hidden_size,), input_shape=(LOOKBACK, hidden_size)))
+gru_model.add(Dense(1, activation='sigmoid'))
+gru_model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=lr, decay=0.0),
+                   metrics=['accuracy', FalsePositives(), FalseNegatives(), 
+                            TruePositives(), TrueNegatives(), 
+                            Recall(), Precision()])
+print(gru_model.summary())
 class_weight = {0: 1.,
-                1: 150.}
-lstm_model.fit(X_train_scaled, y_train, epochs=epochs,
+                1: 1.}
+gru_model.fit(X_train_scaled, y_train, epochs=epochs,
                batch_size=batch,
                #validation_data=(X_valid_scaled, y_valid),
                verbose=2, class_weight=class_weight)
-scores = lstm_model.evaluate(X_test_scaled, y_test, verbose=1)
+scores = gru_model.evaluate(X_test_scaled, y_test, verbose=1)
 print(scores)
+
+# Save the model
+gru_model.save('final_model.ker')
+X = np.array(X)
+X = X.reshape(X.shape[0], LOOKBACK, n_features)
+final_scaler = StandardScaler().fit(flatten(X))
+with open('final_scaler.pickle', 'wb') as wf:
+    pickle.dump(final_scaler, wf)
