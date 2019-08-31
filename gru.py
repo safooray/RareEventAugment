a9f6aee4cb5849ac1fd5aa7b755b8d844a7e092e
Safoora Yousefi <safoora.yousefi1192@gmail.com>
2019-08-31 01:14:58 +0000

    organized augment cases

--- gru.py
+++ gru.py
@@ -1,4 +1,4 @@
-from augmentation import resample_augment
+from augmentation import resample_augment, gradient_augment 
 from data_utils import label_shift, prepare_data
 import numpy as np
 import pandas as pd
@@ -16,31 +16,31 @@ from tensorflow.keras.layers import Dense, GRU, LSTM, Reshape
 SEED = 0
 DATA_SPLIT_PCT = 0.20
 LOOKBACK=20
+ALL_SEQ=True
+AUGMENT_METHOD = 'resample_and_augment'
 
 input_X, input_y = prepare_data()
+if 'gradient' in AUGMENT_METHOD:
+    input_X, input_y = gradient_augment(input_X, input_y, order=1)
 
 n_features = input_X.shape[1]
 
 X, y = temporalize(input_X, input_y, LOOKBACK)
 
-
 X_train, X_test, y_train, y_test = train_test_split(np.array(X), np.array(y), test_size=DATA_SPLIT_PCT, random_state=SEED)
 #X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=DATA_SPLIT_PCT, random_state=SEED)
-
 
 X_train = X_train.reshape(X_train.shape[0], LOOKBACK, n_features)
 #X_valid = X_valid.reshape(X_valid.shape[0], LOOKBACK, n_features)
 X_test = X_test.reshape(X_test.shape[0], LOOKBACK, n_features)
 
 
-X_train, y_train = resample_augment(X_train, y_train)
+if 'resample' in AUGMENT_METHOD:
+    X_train, y_train = resample_augment(X_train, y_train)
+
 X_train, y_train = shuffle(X_train, y_train, random_state=SEED)
 
 print(X_train.shape, X_test.shape)
-x_pos = X_train[y_train == 1]
-frac = X_train.shape[0] // x_pos.shape[0]
-print(frac)
-
 
 scaler = StandardScaler().fit(flatten(X_train))
 X_train_scaled = scale(X_train, scaler)
@@ -48,27 +48,38 @@ X_train_scaled = scale(X_train, scaler)
 X_test_scaled = scale(X_test, scaler)
 
 
-epochs = 1
+epochs = 400
 batch = 128
 lr = 0.0001
 hidden_size = 128
 
 gru_model = Sequential()
-gru_model.add(GRU(units=hidden_size, activation='relu', return_sequences=True,
-               dropout = 0.5, recurrent_dropout=0.5, input_shape=(LOOKBACK, n_features)))
-gru_model.add(Reshape((LOOKBACK*hidden_size,), input_shape=(LOOKBACK, hidden_size)))
+if ALL_SEQ == True:
+    gru_model.add(GRU(units=hidden_size, activation='relu', return_sequences=True,
+                   dropout = 0.5, recurrent_dropout=0.5, input_shape=(LOOKBACK, n_features)))
+    gru_model.add(Reshape((LOOKBACK*hidden_size,), input_shape=(LOOKBACK, hidden_size)))
+else:
+    gru_model.add(GRU(units=hidden_size, activation='relu', return_sequences=False,
+                   dropout = 0.5, recurrent_dropout=0.5, input_shape=(LOOKBACK, n_features)))
 gru_model.add(Dense(1, activation='sigmoid'))
 gru_model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=lr, decay=0.0),
                    metrics=['accuracy', FalsePositives(), FalseNegatives(), 
                             TruePositives(), TrueNegatives(), 
                             Recall(), Precision()])
 print(gru_model.summary())
-class_weight = {0: 1.,
-                1: 1.}
+
+if 'resample' not in AUGMENT_METHOD:
+    class_weight = {0: 1.,
+                    1: 69.}
+else:
+    class_weight = {0: 1.,
+                    1: 1.}
 gru_model.fit(X_train_scaled, y_train, epochs=epochs,
                batch_size=batch,
-               #validation_data=(X_valid_scaled, y_valid),
+               validation_data=(X_test_scaled, y_test),
+               validation_freq=100,
                verbose=2, class_weight=class_weight)
+
 scores = gru_model.evaluate(X_test_scaled, y_test, verbose=1)
 print(scores)
 
